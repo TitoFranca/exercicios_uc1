5960359ac1629ba77d51ad0a37eccdfd4729e906
TitoFranca <tialvesfran@hotmail.com>
2026-03-30 20:21:32 -0300

    Alteração Percentual e Melhoria na linha

--- exercicio_01.py
+++ exercicio_01.py
@@ -1,4 +1,4 @@
-salario_bruto = float(input("Informe o salário:"))
-desconto = salario_bruto*0.2
+salario_bruto = float(input("Informe o salário:\n"))
+desconto = salario_bruto*0.3
 salario_liquido = salario_bruto - desconto
 print(f"Salário Líquido: {salario_liquido:2f}")
