db55db7b93d0a883a04f320f9329a6f273b417ce
TitoFranca <tialvesfran@hotmail.com>
2026-04-01 19:49:01 -0300

    Incompleto

---
+++ Lista03_exercicio5.py
@@ -0,0 +1,20 @@
+# soma = 0
+# for numero in range (85, 907):
+#     modulo_numero = numero % 2
+#     if modulo_numero == 0:
+#         print(f" O número [{numero}] é par")
+#         soma = soma + numero
+# print( f" A soma dos números pares é {soma}")
+
+numeros_par []
+
+print( f"Lista de número pares: {numeros_par}")
+
+# 2a Forma
+
+for numero_par in numeros_par:
+    print( f" O número [{numero_par}]")
+
+# 3a Forma
+tamanho_lista_pares = len(numeros_par)
+for posi
