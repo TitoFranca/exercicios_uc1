92ef5ee68000ecabe4055cb87b781efe5a71b617
TitoFranca <tialvesfran@hotmail.com>
2026-04-01 21:53:44 -0300

    AAA

--- Aleatorio.py
+++ Aleatorio.py
@@ -1,14 +1,16 @@
 listaNomes = [ ]
 listaFamilias = [ ]
-
 qtdNomes = 3
 qtdFamilias = 2
+
 for familia in range(qtdFamilias):
- for nome in range(qtdNomes):
-  nome = input( "Informe um nome de sua familia" )
-  listaNomes.append( nome )
- listaFamilias.append( listaNomes )
- listaNomes = [ ]
+    for nome in range(qtdNomes):
+        nome = input( "Informe um nome de sua familia " )
+        listaNomes.append( nome )
+
+listaFamilias.append( listaNomes )
+listaNomes = [ ]
+
 for item in listaFamilias: 
- print( item )
- print()
+    print( item )
+    print()
