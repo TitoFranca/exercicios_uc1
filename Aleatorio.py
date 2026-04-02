eb401c1331a848e69fb8187e99b9c56d4d56e444
TitoFranca <tialvesfran@hotmail.com>
2026-04-01 21:27:31 -0300

    Loucura 2

---
+++ Aleatorio.py
@@ -0,0 +1,14 @@
+listaNomes = [ ]
+listaFamilias = [ ]
+
+qtdNomes = 3
+qtdFamilias = 2
+for familia in range(qtdFamilias):
+ for nome in range(qtdNomes):
+  nome = input( "Informe um nome de sua familia" )
+  listaNomes.append( nome )
+ listaFamilias.append( listaNomes )
+ listaNomes = [ ]
+for item in listaFamilias: 
+ print( item )
+ print()
