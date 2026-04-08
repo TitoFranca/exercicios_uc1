c660ce7186c5221be358e428c794849d5fcb97fa
TitoFranca <tialvesfran@hotmail.com>
2026-04-08 19:46:53 -0300

    Para Continuar

--- lista03_exercicio08.py
+++ lista03_exercicio08.py
@@ -1,12 +1,12 @@
 
+soma_idade = 0
+
 for i in range (1, 4):
     print("Dados por alunos");
     idade = int(input("Digite a idade "))
     altura = float(input("Digita a altura "))
 
-soma_idade = 0
-
     if altura < 1.70:
         soma_idade = soma_idade + idade
-    print(f"A média de idade dos alunos abaixo de 1.70: {soma_idade}")
+print(f"A média de idade dos alunos abaixo de 1.70: {soma_idade}")
 
